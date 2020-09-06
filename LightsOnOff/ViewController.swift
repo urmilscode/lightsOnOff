341ae1d2c815bb9aefc7e24f9e085fa5ffc1b50e
urmil <ucodes28@gmail.com>
2020-09-06 09:48:13 -0500

    added UIlabel UIButton to storyboard

---
+++ LightsOnOff/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  LightsOnOff
+//
+//  Created by Urmil Shah on 9/6/20.
+//  Copyright © 2020 Spark28 Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    ///MARK: LABEL
+    
+    @IBOutlet weak var lightStatus: UILabel!
+    
+    ///MARK: ACTIONS
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
+
